caf827a54c73411cbfdecf60ae8d229c9b6ddb13
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2025-06-15 12:58:13 +0200

    how it works

--- src/components/HowItWorksCard.jsx
+++ src/components/HowItWorksCard.jsx
@@ -11,7 +11,7 @@ export default function HowItWorksCard({number, title, text, align}) {
     >
       <div className="w-[260px] h-[260px] lg:w-[360px] lg:h-[360px] lg:-ml-20 border border-secondary rounded-full flex flex-col items-center justify-center px-4">
         <div className="flex flex-col items-start text-left text-secondary max-w-[220px]">
-          <h3 className="font-bold text-lg">{number}</h3>
+          <h3 className="font-bold ">{number}</h3>
           <p className="font-bold">{title}</p>
           <p>{text}</p>
         </div>
