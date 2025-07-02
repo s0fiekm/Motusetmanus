f6003da544d5bbcdd65dc3852f1794dedeb71c1f
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2025-07-02 20:35:06 +0200

    !

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,10 @@
-import {createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error(
-    "Supabase environment variables are not set. Check your .env.local file."
-  );
+  console.error("Supabase environment variables are not set.");
 }
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
